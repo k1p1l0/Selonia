09831c5437294cadb056a4a1edff15aa071d8d13
k1p1l0 <s0ht@mail.ru>
2016-09-24 22:39:27 +0300

    separate main script to fail

---
+++ front-end/js/main.js
@@ -0,0 +1,51 @@
+const API_URL = 'https://3j5zptrz4m.execute-api.us-east-1.amazonaws.com/prod/recipients';
+
+$(() => {
+	$.ajax({
+		type: 'GET',
+		url: API_URL,
+
+		success: function(data) {
+			data.Items.forEach((item, i) => {
+				$('.loader').hide();
+				$('#main-table').show();
+				$('#main-table tbody tr:last').after
+				(`
+					<tr>
+						<td>${i+1}</td>
+						<td>${item.name}</td>
+						<td>${item.email}</td>
+						<td>
+							<select name="templates" class="form-control">
+						    <option disabled selected>Choose template</option>
+						    <option value="origin">Origin</option>
+						    <option value="new">New</option>
+					   	</select>
+						</td>
+						<td>
+							<button type="button" class="btn btn-success" disabled>Send</button>
+							<button type="button" class="btn btn-danger" disabled>Delete</button>
+						</td>
+					</tr>
+				`);
+			});
+		}
+	})
+});
+
+$('#addOne').on('click', () => {
+	$.ajax({
+		type: 'POST',
+		url: API_URL,
+		data: JSON.stringify({
+			"recipients": [{
+				"name": $('#nameInput').val(),
+				"email": $('#emailInput').val()
+			}]
+		}),
+		contentType: "application/json",
+		success: function() {
+			location.reload();
+		}
+	});
+});
