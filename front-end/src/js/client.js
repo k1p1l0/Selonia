95099c2b3a7e302d53f1df92fa88c2990728c9f5
k1p1l0 <s0ht@mail.ru>
2016-10-07 16:19:39 +0300

    Logs table & button = logs layout

--- front-end/src/js/client.js
+++ front-end/src/js/client.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, hashHistory} from 'react-router';
 
-import LayoutContainer from './containers/Layout';
-import App from './containers/App';
+import App from './app/App';
+import Layout from './app/Layout';
+import Logs from './app/Logs';
 
 const API_URL = 'https://3j5zptrz4m.execute-api.us-east-1.amazonaws.com/prod';
 
@@ -12,15 +13,15 @@ const $app = $('#app');
 class LayoutWrapper extends React.Component {
   render() {
     return (
-    	<LayoutContainer source={API_URL} />
+    	<Layout source={API_URL} />
     );
   }
 }
 
-class Logs extends React.Component {
+class LogsWrapper extends React.Component {
   render() {
     return (
-			<h1>123</h1>
+			<Logs source={API_URL} />
     );
   }
 }
@@ -29,18 +30,7 @@ ReactDOM.render(
 	<Router history={hashHistory}>
 		<Route path='/' component={App}>
 			<IndexRoute component={LayoutWrapper}></IndexRoute>
-			<Route path='/logs' component={Logs} />
+			<Route path='/logs' component={LogsWrapper} />
 		</Route>
 	</Router>,
 $app[0])
-
-String.prototype.hashCode = function() {
-  var hash = 0, i, chr, len;
-  if (this.length === 0) return hash;
-  for (i = 0, len = this.length; i < len; i++) {
-    chr   = this.charCodeAt(i);
-    hash  = ((hash << 5) - hash) + chr;
-    hash |= 0; // Convert to 32bit integer
-  }
-  return hash;
-};
