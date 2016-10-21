66659b798f5b13b32265992cd3a022eb84e41e79
k1p1l0 <s0ht@mail.ru>
2016-10-21 07:54:22 +0300

    Auth basic

--- client/src/js/client.js
+++ client/src/js/client.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, hashHistory} from 'react-router';
+import { Router, Route, IndexRoute, hashHistory, withRouter} from 'react-router';
 
 import App from './app/App';
 import Layout from './app/Layout';
 import Logs from './app/Logs';
+import Login from './app/Login';
+
+import auth from './auth';
 
 const API_URL = 'https://3j5zptrz4m.execute-api.us-east-1.amazonaws.com/prod';
 
@@ -26,11 +29,28 @@ class LogsWrapper extends React.Component {
   }
 }
 
+// class LoginWrapper extends React.Component {
+//   render() {
+//     return <Login location={this.props.location} router={this.props.router}/>
+//   }
+// }
+
+function requireAuth(nextState, replace) {
+  if (!auth.loggedIn()) {
+    replace({
+      pathname: '/login',
+      state: { nextPathname: nextState.location.pathname }
+    })
+  }
+}
+
 ReactDOM.render(
 	<Router history={hashHistory}>
-		<Route path='/' component={App}>
-			<IndexRoute component={LayoutWrapper}></IndexRoute>
-			<Route path='/logs' component={LogsWrapper} />
-		</Route>
+    <Route path="login" component={Login} />
+
+    <Route path='/' component={App} onEnter={requireAuth}>
+      <IndexRoute component={LayoutWrapper}></IndexRoute>
+      <Route path='/logs' component={LogsWrapper} />
+    </Route>
 	</Router>,
 $app[0])
