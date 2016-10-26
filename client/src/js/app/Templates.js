ef8d38f2b09f190d3927e7c1ab64fcc6334fd0f3
k1p1l0 <s0ht@mail.ru>
2016-10-26 18:03:02 +0300

    Implement GET request for templates page

--- client/src/js/app/Templates.js
+++ client/src/js/app/Templates.js
@@ -1,7 +1,66 @@
 import React from 'react';
 
+import auth from '../auth';
+
 export default class Templates extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			templates: []
+		};
+
+		this.loadTemplates();
+	}
+
+	loadTemplates() {
+    $.ajax({
+      type: 'GET',
+			url: `${this.props.source}/templates`,
+			crossDomain: true,
+			headers: { 
+      	'Authorization':  auth.getToken()
+      },
+
+			beforeSend: function (request) {
+      	request.setRequestHeader("Authorization", auth.getToken());
+      },
+
+			success: function(data) {
+				if (data.Items.length !== this.state.templates.length && !this.UnMount) {
+					this.setState({templates: data.Items})
+				}
+			}.bind(this),
+
+			error: function() {
+				console.log('Some trouble with token!');
+
+				auth.logout();
+				location.reload();
+			}
+    })
+  }
+
+  componentWillMount() {
+		this.UnMount = false;
+	}  
+
+	componentWillUnmount() {
+		this.UnMount = true;
+	}
+
 	render() {
+		var templatesRows = this.state.templates.map(function(value, i) {
+      return (
+				<tr key={i}>
+					<td>{value.name}</td>
+					<td><a href="#">Download</a></td>
+					<td><button class="btn btn-primary">Edit</button></td>
+					<td><button class="btn btn-danger">Delete</button></td>
+				</tr>
+      );
+    }.bind(this));
+
 		return (
 			<div class="table-responsive">
 				<table class="table table-bordered">
@@ -14,12 +73,7 @@ export default class Templates extends React.Component {
 						</tr>
 					</thead>
 					<tbody>
-						<tr>
-							<td>Origin</td>
-							<td><a href="#">Download</a></td>
-							<td><button class="btn btn-primary">Edit</button></td>
-							<td><button class="btn btn-danger">Delete</button></td>
-						</tr>
+						{templatesRows}
 					</tbody>
 				</table>
 			</div>
